432b0a0744481d1734dd6015a9f2e9605db531fc
JhonoBrine <jhonpabroa@gmail.com>
2023-11-19 01:46:21 +0800

    "Changes to AboutPage"

--- src/HomePage/AboutPage.js
+++ src/HomePage/AboutPage.js
@@ -6,22 +6,31 @@ export default function AboutPage() {
       
       <div className="main-container">
         <div className="citulogo-image"></div>
-        <div>
-        <h2 className="about-title">Elevate your Campus Drive with ParkIT</h2>
+        <div className="text-container">
+          <h2 className="about-title">Elevate your Campus Drive with ParkIT</h2>
 
-        <p>
-        Established in 2023, CIT-U proudly presents ParkIT, a brainchild of our
-        visionary creators dedicated to <br/> enhancing your daily campus experience
-        </p>
+          <p>
+          Established in 2023, CIT-U proudly presents ParkIT, a brainchild of our
+          visionary creators dedicated to <br/> enhancing your daily campus experience
+          </p>
 
-        <h3>Innovation by Design</h3>
+          <h3>Innovation by Design</h3>
 
-        <p>
-        Crafted with a keen understanding of the parking management and challenges faced on campus.
-        </p>
+          <p>
+          Crafted with a keen understanding of the parking management and challenges faced on campus.
+          </p>
 
-        <h3>Creators at the Core</h3>
+          <h3>Creators at the Core</h3>
+
+          <div className="profile-container">
+            <div className="profile p1"><img/><span>Jhon Lorenz Pabroa</span></div>
+            <div className="profile p2"><img/><span>April Justine Vega</span></div>
+            <div className="profile p3"><img/><span>Rey Mar Segalle</span></div>
+            <div className="profile p4"><img/><span>Jasmine Lisondra</span></div>
+
+          </div>
         </div>
+        
       </div>
 
      
