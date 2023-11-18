a1d29d017ec39ca8c7bd77db260b4626a1467a78
JhonoBrine <jhonpabroa@gmail.com>
2023-11-19 00:50:42 +0800

    "Added AboutPage"

--- src/HomePage/AboutPage.js
+++ src/HomePage/AboutPage.js
@@ -1,10 +1,30 @@
+import "./styleMain/AboutPage.css";
 
+export default function AboutPage() {
+  return (
+    <>
+      
+      <div className="main-container">
+        <div className="citulogo-image"></div>
+        <div>
+        <h2 className="about-title">Elevate your Campus Drive with ParkIT</h2>
 
-export default function AboutPage(){
+        <p>
+        Established in 2023, CIT-U proudly presents ParkIT, a brainchild of our
+        visionary creators dedicated to <br/> enhancing your daily campus experience
+        </p>
 
-    return(<>
-    
-        <h1>About Page</h1>
-        
-    </>)
+        <h3>Innovation by Design</h3>
+
+        <p>
+        Crafted with a keen understanding of the parking management and challenges faced on campus.
+        </p>
+
+        <h3>Creators at the Core</h3>
+        </div>
+      </div>
+
+     
+    </>
+  )
 }
