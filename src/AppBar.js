6485212a74b715f4dc8b59a382a06423be9689bf
CroseaMors <143801605+CroseaMors@users.noreply.github.com>
2023-11-22 18:08:28 +0800

    Add files via upload

---
+++ src/AppBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Grid } from '@mui/material';
+import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
+import './App.css';
+import campus from './campus.jpg';
+import CreateUser from './CreateUser';
+import SearchUser from './SearchUser';
+import SearchResult from './SearchResult';
+import ParkingSlots from './ParkingSlots';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Box sx={{ width: '90%', bgcolor: 'gold', marginTop: '10px', marginLeft: '5%', borderTopLeftRadius: '10px', borderTopRightRadius: '10px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px'}}>
+          <Tabs centered>
+            <Tab style={{marginRight: '100px', color: 'maroon', fontWeight: 'bolder'}} label={<Link to="/create-user">CREATE USER</Link>} />
+            <p style={{color: 'white', fontWeight: 'bolder'}}> | </p>
+            <Tab style={{marginRight: '100px', marginLeft: '100px', color: 'maroon', fontWeight: 'bolder'}} label={<Link to="/search-user">SEARCH USER</Link>} />
+            <p style={{color: 'white', fontWeight: 'bolder'}}> | </p>
+            <Tab style={{marginRight: '100px', marginLeft: '100px', color: 'maroon', fontWeight: 'bolder'}} label={<Link to="/parking-slots">PARKING SLOTS</Link>} />
+          </Tabs>
+        </Box>
+
+        <Grid className='HomeGrid'container spacing={2} justifyContent="center" style={{width: '90%', marginLeft: '5%'}}>
+        <img className="campus" src={campus} alt="LOGO" />
+          <Grid item xs={12} sm={6}>
+          </Grid>
+        </Grid>
+        <Routes>
+              <Route path="/create-user" element={<CreateUser />} />
+              <Route path="/search-user" element={<SearchUser />} />
+              <Route path="/search-result" element={<SearchResult />} />
+              <Route path="/parking-slots" element={<ParkingSlots/>} />
+            </Routes>
+      </div>
+    </Router>
+    
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./HomePage/styleMain/navBarMainPage.css"
-import { BrowserRouter } from 'react-router-dom';
-import ParkCitRouter from './Routemain';
-
-
-//fonts
-
-import '/public/fonts/OpenSauce/OpenSauceOne-Black.ttf'
-import '/public/fonts/OpenSauce/OpenSauceOne-Regular.ttf'
+// import TheContext from './TheContext';
+import AppBar from './AppBar';
+import CenteredTabs from './AppBar';
+// import TheRef from './TheRef';
+// import TheEffect from './TheEffect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ParkCitRouter/>
-    </BrowserRouter>
+    <App />
+    {/* <TheEffect/> */}
+    {/* <TheRef/> */}
+    {/* <TheContext/>a */}
+     <CenteredTabs /> 
   </React.StrictMode>
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
