2da705e1b31f6c82c8224a323790f4b2814912a8
JhonoBrine <jhonpabroa@gmail.com>
2023-11-30 11:59:35 +0800

    "Added alot of changes in the file"

--- src/AdminPage/ParkingSlots.js
+++ src/AdminPage/ParkingSlots.js
@@ -7,34 +7,34 @@ const ParkingSlots = () => {
     <div className="admin-parkingSlotsContainer">
       <Grid container spacing={2} className="customGrid">
         <Grid item xs={12}>
-          <Box className="customBox" style={{marginLeft: '5%', marginTop: '50px',outline: '5px solid #f6c301'}}>
-            
-            <Grid container style={{ backgroundColor: 'gold', width: '80%', marginLeft: '10%', color:'maroon',marginBottom:'10px'}}>
-              <div>
-
-                <div>
-                <p className='space2'>TOTAL PARKING SLOT: <span>30</span></p>
+          <Box className="customBox" style={{marginLeft: '5%',outline: '5px solid #f6c301', marginTop: '50px'}}>
+            <div className='customSlot-main'>
+              <Grid container style={{ backgroundColor: 'gold', width: '90%', color:'maroon',marginBottom:'10px'}}>
+                  <div className='slot-Bar'>
+                    <ul>
+                      <li className='space2'>TOTAL PARKING SLOT: <span>30</span></li>
+                      <li className='space2'>AVAILABLE PARKING SLOTS: <span>10</span></li>
+                      <li className='space2'>OCCUPIED SLOTS: <span>20</span></li>
+                    </ul>
+                  </div>
+              </Grid>
+                <div className='slot-secondary'>
+                    <div className='nav-leftMain'>
+                      <ul>
+                        <li>ALL</li>
+                        <li>RTL BLDG</li>
+                        <li>NGE BLDG</li>
+                        <li>GLE BLDG</li>
+                        <li>BACK GATE</li>
+                        <li>LIBRARY</li>
+                      </ul>
+                    </div>
+                    <Grid container className='result' style={{ backgroundColor: '#d9d9d9', width: '80%', height: '450px', marginLeft: '10px'}}>
+                      <p style={{ color: 'white' }}>SLOTS HERE</p>
+                    </Grid>  
+                    </div>
+                  
                 </div>
-                <div>
-                <p className='space2'>AVAILABLE PARKING SLOT: <span>10</span></p>
-                </div>
-                <div>
-                <p className='space2'>OCCUPIED SLOTS: <span>20</span></p>
-                </div>
-              
-              </div>
-            </Grid>
-
-            <Grid container className='result' style={{ backgroundColor: 'gray', width: '80%', marginLeft: '10%', height: '50%'}}>
-              <p style={{ color: 'white' }}>SLOTS HERE</p>
-            </Grid>  
-            <div className='buttons'>
-              <div className='SRCancel'>
-                <Button style={{ backgroundColor: 'gold', color: 'maroon', fontWeight: 'bolder' }} variant="contained">
-                  CANCEL
-                </Button>
-              </div>
-            </div>
           </Box>
         </Grid>
       </Grid>
